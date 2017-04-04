5cb61487f6854d9ad493c2da3255aea0f7cde712
Joyounger <942510346@qq.com>
2017-04-04 14:20:46 +0800

    do question 613
    Signed-off-by: Joyounger <942510346@qq.com>

--- 6-嵌入式/613.c
+++ 6-嵌入式/613.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+union
+{
+	struct
+	{
+		unsigned char c1:3;
+		unsigned char c2:3;
+		unsigned char c3:2;
+	} s;
+	unsigned char c;
+} u;
+
+int main(int argc, char const *argv[])
+{
+	u.c = 100;
+	printf("%d\n", u.s.c1);
+	printf("%d\n", u.s.c2);
+	printf("%d\n", u.s.c3);
+
+	return 0;
+}
